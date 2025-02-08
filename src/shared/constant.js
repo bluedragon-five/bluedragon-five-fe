92c265129ab78548275199ac3cba7ace810f73af
Choi-JY1107 <cjy11230@kakao.com>
2025-02-09 02:26:40 +0900

    fix:hotfix

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -3,7 +3,7 @@ export const API_BASE_URL = 'http://3.37.76.244:8080'
 export const API_BASE_HEADERS = {
     'Content-Type': 'application/json',
     'Accept': '*/*',
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Origin': 'https://bluedragon-five-fe.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST',
     'Access-Control-Allow-Headers': 'content-type',
     'Access-Control-Allow-Credentials': 'true'
