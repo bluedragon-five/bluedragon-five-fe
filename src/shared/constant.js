2ba006cbdca07f2f78ba01130743963f65e6d0be
Choi-JY1107 <cjy11230@kakao.com>
2025-02-09 01:44:58 +0900

    fix: API 기본 헤더 수정

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -3,4 +3,9 @@
 export const API_BASE_URL = 'https://3.37.76.244:8080'
 export const API_BASE_HEADERS = {
     'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Methods': 'GET, POST',
+    'Access-Control-Allow-Headers': 'content-type',
+    'Access-Control-Allow-Credentials': 'true'
 }
