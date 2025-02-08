c5c512012910fed78898db712b9ae3a9bdcd34ac
Choi-JY1107 <cjy11230@kakao.com>
2025-02-08 21:06:11 +0900

    feat: 회원가입 페이지 구현

---
+++ src/pages/SignupPage/index.jsx
@@ -0,0 +1,13 @@
+import SignupForm from "./SignupForm"
+import "./style.css"
+
+const SignupPage = () => {
+  return (
+    <div className="signup__page">
+      <h2>회원가입</h2>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default SignupPage
